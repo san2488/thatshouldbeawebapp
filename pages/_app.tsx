088669f871b4e541b14e029c42f0dacfd5e0d4cd
Sujay Narsale <sujay.narsale@gmail.com>
2021-12-26 02:05:54 -0800

    Setup a NextJS website with 5 website data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   return <Component {...pageProps} />
 }
 
